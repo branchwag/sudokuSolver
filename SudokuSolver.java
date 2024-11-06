6a40bbc6f32dad2f1657fd15175ba7f1aa310b44
branchwag <branchleannelmb@gmail.com>
2024-11-05 20:29:39 -0500

    adds solveboard func

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -55,7 +55,28 @@ public class SudokuSolver {
 		return !isNumberInRow(board, number, row) && !isNumberInColumn(board, number, column) && !isNumberInBox(board, number, row, column);
 	}
 
-	//algo will go here
+	private static boolean solveBoard(int[][] board) {
+		for (int row = 0; row < GRID_SIZE; row++) {
+			for (int column = 0; column < GRID_SIZE; column++) {
+				if (board[row][column] == 0) {
+					for (int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++){
+						if (isValidPlacement(board, numberToTry, row, column)) {
+							board[row][column] = numberToTry;
+
+							if(solveBoard(board)) {
+								return true;
+							}
+							else {
+								board[row][column] = 0;
+							}
+						}
+					}
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 
 
 
