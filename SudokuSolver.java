97b133600ecdb2f391cd103ed3cbd759a73f9666
branchwag <branchleannelmb@gmail.com>
2024-11-05 20:43:22 -0500

    prints out board in more readable format

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -26,7 +26,13 @@ public class SudokuSolver {
 
 	private static void printBoard(int[][] board) {
 		for (int row = 0; row < GRID_SIZE; row++) {
+			if (row % 3 == 0 && row != 0) {
+				System.out.println("-----------");
+			}
 			for (int column = 0; column < GRID_SIZE; column++) {
+				if (column % 3 == 0 && column != 0) {
+					System.out.print("|");
+				}
 				System.out.print(board[row][column]);
 			}
 			System.out.println();
